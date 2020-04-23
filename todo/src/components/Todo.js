56409da474e86f76fac6a80416733e81226cb923
jaren <jarensmart@yahoo.com>
2020-04-22 20:49:10 -0400

    App functionality comleted(ADD/TOGGLE/COMPLETED)

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,17 +1,48 @@
 import React, { useState, useReducer } from "react";
-import { initialState, todoReducer } from "../reducers";
+import { todoReducer } from "../reducers/todoReducer";
+
+export const initialState = {
+  item: "",
+  completed: false,
+};
 
 const Todo = () => {
-  const [newTodo, setNewTodo] = useState("");
-  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const [newTodo, setNewTodo] = useState(initialState);
+  const [{ todos }, dispatch] = useReducer(todoReducer, { todos: [] });
 
   const changeHandler = (e) => {
-    setNewTodo(e.target.value);
+    const newItem = {
+      ...newTodo,
+      item: e.target.value,
+    };
+    setNewTodo(newItem);
   };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch({ type: "ADD_TODO", ...newTodo });
+    setNewTodo(initialState);
+  };
+
+  const handleClear = () => dispatch({ type: "CLEAR_TODOS" });
 
   return (
     <div>
-      <h1>Todos Reduced</h1>
+      <form onSubmit={submitHandler}>
+        <input value={newTodo.item} onChange={changeHandler} />
+      </form>
+      {todos.map((text, index) => (
+        <div
+          key={index}
+          onClick={() => dispatch({ type: "TOGGLE_TODO", id: index })}
+          style={{
+            textDecoration: text.completed ? "line-through" : "",
+          }}
+        >
+          {text.item}
+        </div>
+      ))}
+      <button onClick={handleClear}>Clear Todos</button>
     </div>
   );
 };
--- todo/src/reducers/todoReducer.js
+++ todo/src/reducers/todoReducer.js
@@ -1,16 +1,20 @@
-export const initialState = {
-  item: "",
-  completed: false,
-  id: Date.now(),
+export const todoReducer = (state = [], { type, item, completed, id }) => {
+  switch (type) {
+    case "ADD_TODO":
+      return {
+        todos: [...state.todos, { item, completed }],
+      };
+    case "TOGGLE_TODO":
+      return {
+        todos: state.todos.map((todo, index) =>
+          index === id ? { ...todo, completed: !todo.completed } : todo
+        ),
+      };
+    case "CLEAR_TODOS":
+      return {
+        todos: state.todos.filter((todo) => !todo.completed),
+      };
+    default:
+      return state;
+  }
 };
-
-//actions
-
-
-//reducer
-export const todoReducer = (state, action) => {
-    switch() {
-        default:
-        return state;
-    }
-}
